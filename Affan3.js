8b86ecc0686f2cc649ec5c3e9ead30213e1ed32b
maffansaid <103346388+maffansaid@users.noreply.github.com>
2022-11-03 16:28:04 +0700

    Add files via upload

---
+++ Affan3.js
@@ -0,0 +1,8 @@
+const tombol1 = document.getElementById('submit1')
+tombol1.onclick = function(){
+	alert('Okeee Mantaapp !!!')
+}
+const tombol2 = document.getElementById('submit2')
+tombol2.onclick = function(){
+	alert('Terima Kasih Banyak')
+}
