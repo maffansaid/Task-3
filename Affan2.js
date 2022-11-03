e960c4e012c46185918aa38151f712243aa0ea10
maffansaid <103346388+maffansaid@users.noreply.github.com>
2022-11-03 16:26:25 +0700

    Delete Affan2.js

--- Affan2.js
+++
@@ -1,8 +0,0 @@
-const tombol1 = document.getElementById('submit1')
-tombol1.onclick = function(){
-	alert('Okeee Mantaapp !!!')
-}
-const tombol2 = document.getElementById('submit2')
-tombol2.onclick = function(){
-	alert('Terima Kasih Banyak')
-}
